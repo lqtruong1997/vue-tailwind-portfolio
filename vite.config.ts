ca71591952e304fbf12ce2090b3b77c5fae2f5f5
Truong Le <lqtruong1997@gmail.com>
2024-04-12 15:33:33 +0700

    publish source map

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    sourcemap: true,
+  },
   base: './',
   publicDir: 'dist'
 })
